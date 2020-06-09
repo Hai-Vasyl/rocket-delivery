927271f1aaf6a6bba937a51b0acfe14ee8da460e
Vasyl Hai <hai.vasyl20@gmail.com>
2020-06-09 19:33:20 +0300

    Modified index.js for production

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
+const path = require("path")
 
 const app = express()
 app.use(express.json({ extended: true }))
@@ -29,6 +30,10 @@ const start = async () => {
 
     if (process.env.NODE_ENV === "production") {
       app.use(express.static("client/build"))
+
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+      })
     }
 
     app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
